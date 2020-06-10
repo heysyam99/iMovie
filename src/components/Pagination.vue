ed0f90a43d5d47505c9cf8e4a4d1c7e36fc94e4b
heysyam99 <hisyam@aerodyne.co>
2020-06-10 08:49:15 +0800

    init commit component Pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,48 @@
+<template>
+  <vue-ads-pagination
+    :total-items="dataSource.length"
+    :max-visible-pages="5"
+    :items-per-page="20"
+    :page="page"
+    :loading="loading"
+    @page-change="pageChange"
+    @range-change="rangeChange"
+  ></vue-ads-pagination>
+</template>
+
+<script>
+import "@fortawesome/fontawesome-free/css/all.css";
+import "vue-ads-pagination/dist/vue-ads-pagination.css";
+
+import VueAdsPagination from "vue-ads-pagination";
+
+export default {
+  name: "Pagination",
+
+  components: {
+    VueAdsPagination
+  },
+
+  props: {
+    dataSource: { type: Array, default: () => [] }
+  },
+
+  data() {
+    return {
+      loading: false,
+      page: 0
+    };
+  },
+
+  methods: {
+    pageChange(page) {
+      this.page = page;
+      this.$emit("pageChange", page);
+    },
+
+    rangeChange(start, end) {
+      this.$emit("rangeChange", start, end);
+    }
+  }
+};
+</script>
