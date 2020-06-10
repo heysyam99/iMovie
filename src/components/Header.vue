fdcd00ea1bad648e9eacf74c21c622fce769c9f5
heysyam99 <hisyam@aerodyne.co>
2020-06-10 08:48:42 +0800

    refactor (Header.vue) - Refactor code to make header more dynamic base on the route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,39 +2,113 @@
    <div>
       <nav>
          <div class="container">
-            <ul>
+            <ul :class="currentMovie && 'overlay'" class="text-center">
                <li>
-                  <a href="#">HOME</a>
+                  <router-link to="/home">HOME</router-link>
                </li>
                <li>
-                  <a href="#">TOP</a>
+                  <router-link to="/suggestion">SUGGESTION</router-link>
                </li>
                <li>
-                  <a href="#">LOL</a>
+                  <router-link to="/trending">TRENDING</router-link>
                </li>
                <li>
-                  <a href="#">Item 4</a>
-               </li>
-               <li>
-                  <a href="#">SEARCH</a>
+                  <div class="wrap">
+                     <div class="search">
+                        <input
+                           v-on:keyup.enter="search()"
+                           type="text"
+                           class="searchTerm"
+                           placeholder="What are you looking for?"
+                           ref="searchTerm"
+                        />
+                        <button class="searchButton" @click="search()">
+                           <i class="fa fa-search"></i>
+                        </button>
+                     </div>
+                  </div>
                </li>
             </ul>
          </div>
       </nav>
-      <header>
-         <div class="container">
-            <h1 class="text-center text-white text-uppercase">Theater in your home</h1>
-            <h4 class="text-center text-white text-uppercase">Review, Rating</h4>
+      <header
+         class="header"
+         :style="[
+            currentMovie === null || currentMovie === undefined
+               ? {
+                    background:
+                       'radial-gradient(circle, rgba(9, 9, 121, 1) 18%, rgba(2, 0, 36, 1) 95%)',
+                 }
+               : getBG(),
+         ]"
+      >
+         <div class="container" v-if="currentMovie === null || currentMovie === undefined">
+            <h1 v-if="$route.name !== 'search'" class="text-center text-white text-uppercase">
+               Theater in your home
+            </h1>
+            <h2 v-if="$route.name == 'search'" class="text-center text-white text-uppercase">
+               {{ 'Searching for ' + $route.params.keyword }}
+            </h2>
+            <h4
+               v-if="!$route.params.mode && $route.name !== 'search'"
+               class="text-center text-white "
+            >
+               Review, Rating, and Rock!!
+            </h4>
+            <h4 v-if="$route.params.mode === 'suggestion'" class="text-center text-white ">
+               We got your back!
+            </h4>
+            <h4 v-if="$route.params.mode === 'trending'" class="text-center text-white ">
+               Trend's anywhere, everywhere.
+            </h4>
          </div>
       </header>
    </div>
 </template>
 
 <script>
+import API from '../../api'
+
 export default {
    name: 'Header',
-   props: {
-      msg: String,
+   data() {
+      return {
+         currentMode: null,
+         currentID: null,
+         currentMovie: [],
+         currentBg: {},
+      }
+   },
+
+   watch: {
+      $route({ params }) {
+         this.currentMode = params?.type ? params.type : null
+         this.currentID = params?.id ? params.id : null
+         if (!params?.type || params?.keyword) {
+            this.currentMovie = null
+         }
+      },
+
+      currentMode(newValue) {
+         API.get(`${newValue}/${this.currentID}`).then(({ status, data }) => {
+            if (status === 200) {
+               this.currentMovie = data
+            }
+         })
+      },
+   },
+
+   methods: {
+      getBG() {
+         return {
+            background: `url(https://image.tmdb.org/t/p/original/${this.currentMovie?.backdrop_path})`,
+         }
+      },
+
+      search() {
+         this.$router.push({ path: `/search/keyword/${this.$refs.searchTerm.value}` })
+         this.$refs.searchTerm.value = ''
+      },
    },
 }
 </script>
@@ -92,10 +166,6 @@ nav li {
    transition: all 0.2s;
 }
 
-nav li:last-child {
-   padding-right: 0;
-}
-
 nav a {
    color: #fff;
    text-decoration: none;
@@ -104,6 +174,14 @@ nav a {
 
 nav a:hover {
    color: #ddd;
+}
+
+.overlay {
+   background: rgba(0, 0, 0, 0.6);
+}
+
+.overlay > li {
+   padding: 1.3em !important;
 }
 
 #icon {
@@ -116,7 +194,6 @@ nav a:hover {
 
 header {
    position: relative;
-   background: radial-gradient(circle, rgba(9, 9, 121, 1) 18%, rgba(2, 0, 36, 1) 95%);
    padding: 10em 0 8em;
    z-index: 2;
 }
@@ -128,12 +205,48 @@ header h1 {
 ul {
    list-style-type: none;
    padding: 0;
+   width: 100%;
 }
+
 li {
    display: inline-block;
    margin: 0 10px;
 }
+
 a {
    color: #42b983;
+}
+
+.search {
+   width: 100%;
+   position: relative;
+   display: flex;
+}
+
+.searchTerm {
+   width: 100%;
+   border: 3px solid #00b4cc;
+   border-right: none;
+   padding: 5px;
+   height: 36px;
+   border-radius: 5px 0 0 5px;
+   outline: none;
+   color: #9dbfaf;
+}
+
+.searchTerm:focus {
+   color: #32acc1;
+}
+
+.searchButton {
+   width: 40px;
+   height: 36px;
+   border: 1px solid #32acc1;
+   background: #32acc1;
+   text-align: center;
+   color: #fff;
+   border-radius: 0 5px 5px 0;
+   cursor: pointer;
+   font-size: 20px;
 }
 </style>
