0b7b8b73f230e8e82c01672a72759a647bff364d
heysyam99 <hisyam@aerodyne.co>
2020-06-10 08:50:47 +0800

    refactor (Home.vue) - Refactor code and make the page dynamically load data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <section id="about">
+  <section id="section">
     <div class="container">
       <div class="d-flex">
         <h2>Suggestion</h2>
-        <a style="line-height: 40px; margin-left:10px">See All</a>
+        <a style="line-height: 40px; margin-left:10px" @click="seeAll('suggestion')">See All</a>
       </div>
       <div class="row movie-list">
         <div v-if="!suggestionLoaded" class="spinner-grow" role="status">
@@ -32,7 +32,7 @@
     <div class="container" style="margin-top: 35px">
       <div class="d-flex">
         <h2>Trending</h2>
-        <a style="line-height: 40px; margin-left:10px">See All</a>
+        <a style="line-height: 40px; margin-left:10px" @click="seeAll('trending')">See All</a>
       </div>
       <div class="row movie-list">
         <div
@@ -44,10 +44,10 @@
             <div class="status">
               <div class="rating">{{ movieRating(trend.vote_average) }}</div>
             </div>
-            <a href class="poster">
+            <a class="poster" @click="posterClick(trend)">
               <img :src="getPosterURL(trend.poster_path)" />
             </a>
-            <a href class="name">{{ trend.title }}</a>
+            <a class="name">{{ trend.title }}</a>
           </div>
         </div>
       </div>
@@ -100,125 +100,19 @@ export default {
       return `https://image.tmdb.org/t/p/original/${path}`;
     },
 
-    posterClick({ id }) {
-      this.$router.push({ path: `/movie/${id}` });
+    posterClick({ id, media_type }) {
+      this.$router.push({
+        path: `/${media_type ? media_type : "movie"}/${id}`
+      });
+    },
+
+    seeAll(type) {
+      if (type === "suggestion") {
+        this.$router.push({ path: `suggestion` });
+      } else {
+        this.$router.push({ path: `trending` });
+      }
     }
   }
 };
 </script>
-
-<style scoped>
-a {
-  text-decoration: none;
-  background-color: transparent;
-}
-
-#about {
-  background: #fff;
-  padding: 5em 0;
-}
-#about p {
-  line-height: 2em;
-  font-size: 1.4em;
-  margin: 0;
-  color: #222;
-}
-
-.row {
-  margin-left: -10px;
-  margin-right: -10px;
-}
-
-.movie-list .item {
-  position: relative;
-  margin: 10px 0;
-  width: 200px;
-}
-
-.movie-list .item .poster:before {
-  transition: top 0.5s ease 0s, left 0.5s ease 0s, right 0.5s ease 0s,
-    bottom 0.5s ease 0s, opacity 0.5s ease-out;
-  opacity: 0;
-  background: rgba(0, 0, 0, 0.3);
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  content: "";
-}
-
-.movie-list .item .poster img {
-  width: 100%;
-  height: 100%;
-}
-
-.movie-list .item .poster {
-  -webkit-border-radius: 2.5px;
-  -khtml-border-radius: 2.5px;
-  -moz-border-radius: 2.5px;
-  -ms-border-radius: 2.5px;
-  -o-border-radius: 2.5px;
-  border-radius: 2.5px;
-  overflow: hidden;
-  position: relative;
-  display: block;
-  height: 210px;
-}
-
-.movie-list .item .status {
-  position: absolute;
-  top: 3px;
-  right: 3px;
-  z-index: 1;
-  color: #fff;
-  font-weight: 700;
-}
-
-.movie-list .item .status .rating {
-  background: #00acc1;
-  color: #fff;
-}
-
-.movie-list .item .status > div {
-  font-size: 0.85rem;
-  float: left;
-  padding: 3px 5px;
-  -webkit-border-radius: 2px;
-  -khtml-border-radius: 2px;
-  -moz-border-radius: 2px;
-  -ms-border-radius: 2px;
-  -o-border-radius: 2px;
-  border-radius: 2px;
-}
-
-.movie-list .item .name {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  padding: 5px;
-  color: #fff;
-  text-align: center;
-  font-weight: 400;
-  font-size: 1rem;
-}
-
-@media screen and (min-width: 1599px) {
-  .movie-list .item .poster {
-    height: 240px !important;
-  }
-}
-
-@media screen and (min-width: 767px) {
-  .movie-list .item .poster {
-    height: 200px !important;
-  }
-}
-
-@media screen and (min-width: 700px) {
-  .movie-list .item .poster {
-    height: 240px !important;
-  }
-}
-</style>
