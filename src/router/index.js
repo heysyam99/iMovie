b28255691bb35478c7aa99d008d5038b76ae512c
heysyam99 <hisyam@aerodyne.co>
2020-06-10 08:49:43 +0800

    index.js - Add new route and change the params name of route movie

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -15,12 +14,28 @@ const routes = [
       component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue'),
    },
    {
-      path: '/movie/:id',
+      path: '/:type/:id',
       name: 'movie',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ '../views/Movie.vue'),
+   },
+   {
+      path: '/:mode',
+      name: 'suggestion',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/Suggestion.vue'),
+   },
+   {
+      path: '/search/keyword/:keyword',
+      name: 'search',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue'),
    },
 ]
 
