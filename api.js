ea0c8d0e7ad2694f33c7d26ed5da1c57e2bb3631
heysyam99 <hisyam@aerodyne.co>
2020-06-14 04:37:25 +0800

    refactor (api.js) - Refactor the current code and remove Promises

--- api.js
+++ api.js
@@ -2,26 +2,23 @@ import axios from 'axios'
 
 export default class API {
    static get(module, params) {
-      return new Promise((resolve, reject) => {
-         try {
-            const opts = this._opts(module, params, 'get')
-            const response = axios(opts)
-            resolve(response)
-         } catch (err) {
-            reject(err)
-         }
-      })
+      if (module === null) return
+      try {
+         const opts = this._opts(module, params, 'get')
+         return axios(opts)
+      } catch (err) {
+         return err
+      }
    }
 
    static _opts(module, params, method) {
       const opts = {
          method: method,
          url: `${this.apiUrl()}/${module}`,
-      }
-
-      opts.params = {
-         ...params,
-         api_key: '5fc8fb4fe77ae40e0147a80610031b87',
+         params: {
+            ...params,
+            api_key: '5fc8fb4fe77ae40e0147a80610031b87',
+         },
       }
 
       return opts
